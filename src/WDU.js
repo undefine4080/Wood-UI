56fb26acd31546c3931fa1cab05d9b8123d7c242
Tim <austerlitz6860@126.com>
2021-03-07 14:53:03 +0800

    实现代码复用  重构生成所需要的HTML元素的方法

--- src/WDU.js
+++ src/WDU.js
@@ -5,25 +5,27 @@ class WDU {
     /**
      * 生成所需要的HTML元素
      * 
-     * @param {Array} classNames 需要生成的 div 的类名 
+     * @param {Array} elements elements元素中：第一项-需要生成的html元素的名称，第二项-生成的html元素的类名
      */
-    genBoxes(classNames){
+    genHTML(elements){
         let BOXES = {}
 
-        classNames.forEach(divName => {
-            const div = document.createElement('div')
-            const boxName = `${divName}`
-            div.setAttribute('class', `${this.PREFIX}-${divName}`)
-            BOXES[boxName] = div
+        elements.forEach(element => {
+            const boxName = element[1]
+            const ele = document.createElement(element[0])
+            const className = element[1]
+            ele.setAttribute('class', `${this.PREFIX}-${className}`)
+            BOXES[boxName] = ele
         })
 
         return BOXES
     }
 
     /**
+     * 单例元素的初始化方法
      * 
-     * @param {string} prefix 当前元素类名
-     * @param {function} exe 需执行的方法
+     * @param prefix 当前元素类名, 
+     * @param exe 需执行的方法
      */
     init(prefix, exe) {
         const all = Array.from(document.querySelectorAll(`.${prefix}`))
