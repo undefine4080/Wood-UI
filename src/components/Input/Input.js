6dd8b7388ac01d21cc5235410910a324a4b6e185
Tim <austerlitz6860@126.com>
2021-03-07 14:54:56 +0800

    优化 input 的实例化逻辑

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,51 +1,44 @@
 import './input.less'
+import { WDU } from '../../WDU'
 
-class Input {
+class Input extends WDU {
     constructor () {
+        super()
         this.PREFIX = 'wdu-input'
-        this.init()
-    }
-
-    /**
-    * 初始化Button元素
-    * @param {none}
-    */
-    init() {
-        const all = Array.from(document.querySelectorAll(`.${this.PREFIX}`))
-        all.forEach(one => {
-            this.genDom(one)
-        })
+        this.genDom = this.genDom.bind(this)
+        super.init(this.PREFIX, this.genDom)
     }
 
     genDom(ele) {
-        const input = document.createElement('input')
-        const label = document.createElement('label')
-        input.classList.add(`${this.PREFIX}-input`)
-        label.setAttribute('class', `${this.PREFIX}-label`)
-        ele.appendChild(label)
-        ele.appendChild(input)
+        const needHtml = [['label', 'label'], ['input', 'input']]
+        const BOXES = super.genHTML(needHtml)
+
+        ele.appendChild(BOXES['label'])
+        ele.appendChild(BOXES['input'])
+
         this.setOption(ele)
     }
 
     setOption(ele) {
         const input = ele.lastChild
         const option = JSON.parse(ele.dataset.option)
+
         if (ele.dataset.input) {
             const inputOption = JSON.parse(ele.dataset.input)
             Object.entries(inputOption).forEach(item => {
-                input.setAttribute(item[0],item[1])
+                input.setAttribute(item[0], item[1])
             })
         }
 
-        if(option.type){
-            input.setAttribute('type',option.type)
-        }else{
-            input.setAttribute('type',"text")
+        if (option.type) {
+            input.setAttribute('type', option.type)
+        } else {
+            input.setAttribute('type', "text")
         }
-        
-        if(option.size){
+
+        if (option.size) {
             ele.classList.add(`${this.PREFIX}-${option.size}`)
-        }else{
+        } else {
             ele.classList.add(`${this.PREFIX}-normal`)
         }
 
